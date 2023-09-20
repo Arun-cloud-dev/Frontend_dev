c14c3e5af222f21e987ff57375b54bed25ba2e40
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2023-09-20 15:06:41 +0530

    Add files via upload

---
+++ Java script/Code bank/class 4/ERROR/index.js
@@ -0,0 +1,14 @@
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph)
+
+
+function purchase() {
+    console.log("button clicked");
+
+    errorParagraph.textContent = "Something went Worng , So Please dont try again !";
+}
+
+
+
+
+
