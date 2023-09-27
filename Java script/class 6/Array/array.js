fd1161685cfdb1b5b8e120932ffec3fc0c7b09fa
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2023-09-27 22:59:38 +0530

    class 6

---
+++ Java script/class 6/Array/array.js
@@ -0,0 +1,89 @@
+// Store Multiple Values in one Variable
+
+// const sandwich = ["peanut butter", "jelly", "bread"];
+// console.log(sandwich)
+
+
+// myArray should be an array.
+// The first item in myArray should be a string.
+// The second item in myArray should be a number.
+
+// const myArray = ["helloarun", 5];
+// console.log(myArray)
+
+
+
+
+
+// ************
+// Nest one Array within Another Array
+
+// const teams = [["Bulls", 23], ["white sox", 45],];
+// console.log(teams)
+
+
+
+// Create a nested array called myArray.
+// myArray should have at least one array nested within another array.
+
+// const myArray = [["arun"], ["raj"]];
+// console.log(myArray)
+
+
+
+
+// *****************
+    
+    
+// Access Array Data with Indexes 
+
+// We can access the data inside arrays using indexes.
+// Array indexes are written in the same bracket notation that strings use, 
+//except instead of specifying a character, they are specifying an entry in the array.
+//Like strings, arrays use zero - based indexing, so the first element in an array has an index of 0.
+
+// const array = [50, 60, 70];
+// const data = array[3]
+
+// console.log(data)
+
+
+
+// --------
+
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+// The variable myData should equal the first value of myArray.
+// The data in variable myArray should be accessed using bracket notation.
+
+
+// Setup
+// var myArray = [500, 600, 700];
+
+// var myData = myArray[2];
+// console.log(myData)
+
+// **************
+// Modify Array Data With Indexes
+
+// Example
+// var ourArray = [18, 64, 99];
+// ourArray[2] = 45;
+// console.log(ourArray)
+
+
+
+// Modify the data stored at index 0 of myArray to a value of 45.
+// myArray should now be [45, 64, 99].
+// You should be using correct index to modify the value in myArray.
+
+// Setup
+// var myArray = [18, 64, 99];
+
+// myArray[0] = 45;
+// console.log(myArray)
+
+// *********************
+
+// Access Multi-Dimensional Arrays With Indexes
+
+// go to access- multi array js file 
---
+++ Java script/class 6/loops/typesofloops.js
@@ -0,0 +1,51 @@
+// for loop 
+
+//syntax
+// for (initialization; condition; increment ){
+
+//     //code to be exicuted 
+// }
+
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+
+
+// while loop 
+
+// syntax 
+
+// while (condition) {
+//     //code to be exicuted 
+// }
+
+// let i = 1;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while loop 
+
+// syntax
+ 
+
+// do {
+//     //code to be exicuted 
+// } while (condition);
+
+
+// let i = 1;
+
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i <= 0);
+
+
